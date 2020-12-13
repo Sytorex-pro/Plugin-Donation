b97cd251a602cec8e569aaed34accfc60e3fb675
LaSkyMania <48296837+LaSkyMania@users.noreply.github.com>
2020-12-13 14:41:30 +0100

    Update routes.php

--- Config/routes.php
+++ Config/routes.php
@@ -1,2 +1,4 @@
 <?php
 Router::connect('/donation', ['controller' => 'donation', 'action' => 'index', 'plugin' => 'donation']);
+Router::connect('/donation/canceled', ['controller' => 'donation', 'action' => 'canceled', 'plugin' => 'donation']);
+Router::connect('/donation/return', ['controller' => 'donation', 'action' => 'return', 'plugin' => 'donation']);
